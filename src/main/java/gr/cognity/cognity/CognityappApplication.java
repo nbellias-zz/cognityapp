44b5f42e9b7e89ddb81e2bb65320b044437cbc72
Nikolaos Bellias <nikolaos.bellias@gmail.com>
2019-11-29 09:55:46 +0200

    Initial Commit

---
+++ src/main/java/gr/cognity/cognity/CognityappApplication.java
@@ -0,0 +1,13 @@
+package gr.cognity.cognity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognityappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CognityappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr/cognity/cognity/CognityappApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.cognity.cognity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CognityappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
